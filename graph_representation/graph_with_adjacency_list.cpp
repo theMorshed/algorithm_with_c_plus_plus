7e0c63a625db941754d969dc299717733deb2e30
theMorshed <themorshedctg@gmail.com>
2023-03-15 11:11:21 +0600

    graph with adjacency list code added

--- graph_representation/graph_with_adjacency_list.cpp
+++ graph_representation/graph_with_adjacency_list.cpp
@@ -7,20 +7,21 @@ int main()
     // m is the number of edges
     int n, m;
     cin >> n >> m;
-    int adjMat[n + 1][n + 1] = {};
+    vector<int> adjList[n + 1];
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        adjMat[u][v] = 1;
-        adjMat[v][u] = 1;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
         // for a directed graph with an edge pointing from u
         // to v,we just assign adjMat[u][v] as 1
     }
 
     // display graph matrix
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << adjMat[i][j] << " ";
+        cout << i << " -> ";
+        for (int j = 0; j < adjList[i].size(); j++) {
+            cout << adjList[i][j] << " ";
         }
         cout << endl;
     }
