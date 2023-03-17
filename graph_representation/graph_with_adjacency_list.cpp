f4536ed023a58c51e9ae9f47344c81812e88bd4f
theMorshed <themorshedctg@gmail.com>
2023-03-17 23:41:13 +0600

    graph representation change some code

--- graph_representation/graph_with_adjacency_list.cpp
+++ graph_representation/graph_with_adjacency_list.cpp
@@ -3,25 +3,21 @@ using namespace std;
 
 int main()
 {
-    // n is the number of vertices
-    // m is the number of edges
-    int n, m;
-    cin >> n >> m;
-    vector<int> adjList[n + 1];
-    for (int i = 0; i < m; i++) {
+    int nodes, edges;
+    cin >> nodes >> edges;
+    vector<int> adjList[nodes + 1];
+    for (int i = 0; i < edges; i++) {
         int u, v;
         cin >> u >> v;
         adjList[u].push_back(v);
         adjList[v].push_back(u);
-        // for a directed graph with an edge pointing from u
-        // to v,we just assign adjMat[u][v] as 1
     }
 
-    // display graph matrix
-    for (int i = 0; i < n; i++) {
+    // display graph list
+    for (int i = 0; i < nodes; i++) {
         cout << i << " -> ";
-        for (int j = 0; j < adjList[i].size(); j++) {
-            cout << adjList[i][j] << " ";
+        for (int child: adjList[i]) {
+            cout << child << " ";
         }
         cout << endl;
     }
--- graph_representation/graph_with_adjacency_matrix.cpp
+++ graph_representation/graph_with_adjacency_matrix.cpp
@@ -3,23 +3,19 @@ using namespace std;
 
 int main()
 {
-    // n is the number of vertices
-    // m is the number of edges
-    int n, m;
-    cin >> n >> m;
-    int adjMat[n + 1][n + 1] = {};
-    for (int i = 0; i < m; i++) {
+    int nodes, edges;
+    cin >> nodes >> edges;
+    int adjMat[nodes + 1][nodes + 1] = {};
+    for (int i = 0; i < edges; i++) {
         int u, v;
         cin >> u >> v;
         adjMat[u][v] = 1;
         adjMat[v][u] = 1;
-        // for a directed graph with an edge pointing from u
-        // to v,we just assign adjMat[u][v] as 1
     }
 
     // display graph matrix
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < nodes; i++) {
+        for (int j = 0; j < nodes; j++) {
             cout << adjMat[i][j] << " ";
         }
         cout << endl;
