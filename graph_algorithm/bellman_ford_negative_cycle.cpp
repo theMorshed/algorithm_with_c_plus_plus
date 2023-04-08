6d7f401f1ee748b2d8ed879e2d2b30c4d2bea2d9
theMorshed <themorshedctg@gmail.com>
2023-04-09 01:28:09 +0600

    negative cycle detection with bellman ford algorithm

---
+++ graph_algorithm/bellman_ford_negative_cycle.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+const int INF = 1e9;
+int d[N];
+
+vector<pair<int, int>> adj_list[N];
+
+int main()
+{
+    int nodes, edges;
+    cin >> nodes >> edges;
+    
+    for (int i = 1; i <= nodes; i++) {
+        d[i] = INF;
+    }
+
+    for (int i = 0; i < edges; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj_list[u].push_back({v, w});
+    }
+
+    int src = 1;
+    d[src] = 0;
+
+    bool negative_cycle = false;
+
+    for (int i = 1; i <= nodes; i++) {
+        for (int node = 1; node <= nodes; node++) {
+            for (pair<int, int> adj_node: adj_list[node]) {
+                int u = node;
+                int v = adj_node.first;
+                int w = adj_node.second;
+                
+                if (d[u] + w < d[v]) {
+                    d[v] = d[u] + w;
+
+                    if (i == nodes) {
+                        negative_cycle = true;
+                    }
+                }
+            }
+        }   
+    }
+    
+    if (!negative_cycle) {
+        for (int i = 1; i <= nodes; i++) {
+            cout << d[i] << " ";
+        }
+        cout << endl;
+    } else {
+        cout << "Negative cycle found" << endl;
+    }
+
+    return 0;
+}
